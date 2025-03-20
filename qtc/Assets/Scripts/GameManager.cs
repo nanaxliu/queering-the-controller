ee61a4588d6875e22037448e61d24023350cd161
toastyghostii <kikithecoolcookie@gmail.com>
2025-03-20 16:16:03 -0400

    increased silent mode cooldown

--- qtc/Assets/Scripts/GameManager.cs
+++ qtc/Assets/Scripts/GameManager.cs
@@ -99,10 +99,14 @@ public class GameManager : MonoBehaviour
     void EnableSilentMode()
     {
         silentMode = true;
-        shhText.enabled = false;
 
         ChangeState(GameState.SplitScreen);
         Debug.Log("silent mode");
+    }
+
+    void NoShhText()
+    {
+        shhText.enabled = false;
     }
 
     public void ChangeState(GameState newState)
@@ -116,8 +120,12 @@ public class GameManager : MonoBehaviour
                 platformAnim.Play("platform-shake");
                 shhText.enabled = true;
 
+                timerText.gameObject.SetActive(true);
+                seconds = 15;
+                SilentSeconds();
 
-                Invoke("EnableSilentMode", 1.5f);
+                Invoke("NoShhText", 1.5f);
+                Invoke("EnableSilentMode", 15f);
             }
         }
         else
